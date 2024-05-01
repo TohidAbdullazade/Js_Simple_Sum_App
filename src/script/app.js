e0b1bababc2c8d018dda797cdbc85b1a85cbce88
TohidAbdullazade <tohidabdullazade@gmail.com>
2024-05-01 23:14:07 +0400

    I write a simply calculate app with 2 number

---
+++ src/script/app.js
@@ -0,0 +1,9 @@
+// ===> Variables <=== 
+let numberOne = +prompt("Enter the first number");
+let numberTwo = +prompt("Enter the second number");
+let sum = numberOne + numberTwo;
+
+
+// // ===> Show Function <===
+alert(`The sum is: ${sum}`);
+
